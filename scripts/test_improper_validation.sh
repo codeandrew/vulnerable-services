7dbbcd9bf73c421f7827294d239b2ab117b03e04
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-08-22 19:52:57 +0800

    update test scripts

---
+++ scripts/test_improper_validation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# URL of your vulnerable Flask application's endpoint
+URL="http://localhost:5000/improper-validation"
+
+# Test with a valid multiplier
+echo "Testing with valid multiplier:"
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"command":"echo test","multiplier":"5"}' \
+    $URL
+echo -e "\n"
+
+# Test with an invalid multiplier (e.g., string instead of a number)
+echo "Testing with invalid multiplier (string):"
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"command":"echo test","multiplier":"invalid_multiplier"}' \
+    $URL
+echo -e "\n"
+
+# Test with other unexpected values (e.g., a negative number)
+echo "Testing with unexpected value (negative number):"
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"command":"echo test","multiplier":"-10"}' \
+    $URL
+echo -e "\n"
