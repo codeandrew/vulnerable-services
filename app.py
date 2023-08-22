536eb69578f57bffb969d9b8f676aa630a2cefef
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-08-22 19:04:10 +0800

    update app

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from flask import Flask, request
+from subprocess import check_output
 import sqlite3
 import os
 
 app = Flask(__name__)
 
+
 @app.route('/command', methods=['POST'])
 def command_injection():
     command = request.form['command']
-    os.system(command)
-    return 'Command executed!'
+    try:
+        result = check_output(command, shell=True).decode('utf-8')
+        return f'Command executed! Result: {result}'
+    except Exception as e:
+        return str(e)
 
 @app.route('/sql', methods=['GET'])
 def sql_injection():
     query = request.args.get('query')
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    c.execute(query)
-    return 'Query executed!'
+    try:
+        c.execute(query)
+        result = c.fetchall()
+        return f'Query executed! Result: {result}'
+    except sqlite3.Error as e:
+        return str(e)
+
 
 @app.route('/path', methods=['GET'])
 def path_traversal():
