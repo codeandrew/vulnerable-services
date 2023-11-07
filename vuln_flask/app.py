f9ff6899bca89ff7b480370684035f0214c544da
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-11-07 16:15:47 +0800

    Update app.py

--- vuln_flask/app.py
+++ vuln_flask/app.py
@@ -69,4 +69,8 @@ def execute_payload():
     return 'File executed!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0", 
+        port=5000, 
+        debug=True
+    )
